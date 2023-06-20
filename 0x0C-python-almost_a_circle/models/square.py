9cbfdfe2b5622a7219520e0cca310ca351e98bf6
fafi-make <tebibu374@gmail.com>
2023-06-20 01:00:05 -0700

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-""" New class Square """
+# square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a square class."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Class Square that inherist from Rectangle """
+    """Represent a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ Class constructor """
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
         super().__init__(size, size, x, y, id)
-        self.size = size
 
-    """ Returns printable string representation
-    of an instance """
-    def __str__(self):
-        return ("[{}] ({}) {:d}/{:d} - {:d}".format(
-            self.__class__.__name__, self.id, self.x,
-            self.y, self.size))
-
-    """ Size getter and setter """
     @property
     def size(self):
-        return (self.width)
+        """Get/set the size of the Square."""
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -29,22 +30,56 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
-        """ This method assigns an argument to each attribute """
-        if args:
-            for argmts in range(len(args)):
-                if argmts == 0:
-                    self.id = args[argmts]
-                if argmts == 1:
-                    self.size = args[argmts]
-                if argmts == 2:
-                    self.x = args[argmts]
-                if argmts == 3:
-                    self.y = args[argmts]
+        """Update the Square.
 
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
-        """ Returns the dictionary representation of a Rectangle"""
-        return ({'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y})
+        """Return the dictionary representation of the Square."""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
