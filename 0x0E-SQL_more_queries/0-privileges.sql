da5598f0085eb05789dc1484aad60b6f064837ae
fafi-make <tebibu374@gmail.com>
2023-07-19 03:49:01 -0700

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- List all privileges for users 'user_0d_1' and 'user_0d_2'
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Show privileges
+-- Show privileges for `user_0d_1`
+SHOW GRANTS FOR user_0d_1@localhost;
+-- Show privileges for `user_0d_2`
+SHOW GRANTS FOR user_0d_2@localhost;
